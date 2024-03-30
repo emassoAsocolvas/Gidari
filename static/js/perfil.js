e2619d6c3df6afae7e877f3f9afc612939d89877
PedroDB25 <pedro-0025@hotmail.com>
2024-03-30 16:35:54 +0100

    add: funcionalidad porcentajes

--- static/js/perfil.js
+++ static/js/perfil.js
@@ -12,24 +12,35 @@ function init() {
     modificarBarra(barraEducacion, 0)
 }
 
-function cargarEventosCheckBox(){
+function cargarEventosCheckBox() {
     [...document.querySelectorAll("#ToDo input")].forEach(el => el.addEventListener("input", recargarBarras))
 }
 
-function modificarBarra(nodo, valor){
+function modificarBarra(nodo, valor) {
     nodo.style.width = valor + "%";
     nodo.innerHTML = valor + "%"
 
 }
-function recargarBarras(){
+function recargarBarras() {
 
-    let contador = 0
-    
     let barraEmpadronamiento = document.querySelector("#bar-Empadronamiento .progress-bar")
     let barraSalud = document.querySelector("#bar-Salud .progress-bar")
     let barraEducacion = document.querySelector("#bar-Educacion .progress-bar")
 
-    
+    //Total en empadronamiento
+    calcularPorcentajes("Empadronamiento", barraEmpadronamiento)
+    //Total en salud
+    calcularPorcentajes("Salud", barraSalud)
+    //Total en educacion
+    calcularPorcentajes("Educacion", barraEducacion)
 
+}
+
+function calcularPorcentajes(nombre, nodo) {
+    let inputs = document.querySelectorAll("div#ToDo div#" + nombre + " input")
+    let totalInputs = inputs.length
+    let inputsCheckeados = [...inputs].map(el => el.checked).reduce((a, b) => a + b, 0)
+    let porcentaje = (inputsCheckeados / totalInputs) * 100
+    modificarBarra(nodo, Number((porcentaje).toFixed(2)))
 
 }
