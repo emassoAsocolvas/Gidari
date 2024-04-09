814da98bb74fb81527b08b5b7ea6d4616cdfed6d
PedroDB25 <pedro-0025@hotmail.com>
2024-04-09 16:00:21 +0200

    base perfil

--- static/js/perfil.js
+++ static/js/perfil.js
@@ -13,7 +13,8 @@ function init() {
 }
 
 function cargarEventosCheckBox() {
-    [...document.querySelectorAll("#ToDo input")].forEach(el => el.addEventListener("input", recargarBarras))
+    [...document.querySelectorAll("#ToDo input")].forEach(el => el.addEventListener("input", recargarBarras));
+    [...document.querySelectorAll("#ToDo input")].forEach(el => el.addEventListener("input", cargarTexto));
 }
 
 function modificarBarra(nodo, valor) {
@@ -43,4 +44,78 @@ function calcularPorcentajes(nombre, nodo) {
     let porcentaje = (inputsCheckeados / totalInputs) * 100
     modificarBarra(nodo, Number((porcentaje).toFixed(2)))
 
+}
+function cargarTexto(e) {
+    let marcoParaTexto1 = document.querySelector("div#texto")
+    let mapTextos = getTextos()
+    let idNodo = e.target.id
+
+    if (!e.target.checked) {
+        let nodo = idNodo.split("paso")
+        if (nodo[1] - 1 == 0) {
+            marcoParaTexto1.innerHTML = mapTextos["inicio"]
+            return
+        }
+        marcoParaTexto1.innerHTML = mapTextos[nodo[0] + "paso" + (nodo[1] - 1)]
+        return
+    } else {
+        let texto = mapTextos[idNodo]
+        marcoParaTexto1.innerHTML = texto
+    }
+
+    let segundoTexto = getSegundoTextos()
+    let marcoParaTexto2 = document.querySelector("div#links")
+    if (!e.target.checked) {
+        let nodo = idNodo.split("paso")
+        if (nodo[1] - 1 == 0) {
+            marcoParaTexto2.innerHTML = segundoTexto["inicio"]
+            return
+        }
+        marcoParaTexto2.innerHTML = segundoTexto[nodo[0] + "paso" + (nodo[1] - 1)]
+        return
+    } else {
+        let texto = segundoTexto[idNodo]
+        marcoParaTexto2.innerHTML = texto
+    }
+}
+function getTextos() {
+    return {
+        "inicio": "Da click a los paso de abajo para ver mas información",
+        "empadronamiento-paso1": "Cada ayuntamiento puede tener requisitos específicos, puede consultar los datos del Ayuntamiento del municipio en que reside en",
+        "empadronamiento-paso2": "Selecciona tu municipio para acceder a realizar la reserva",
+        "empadronamiento-paso3": "¿Para cuándo tendrás la cita? Te ayudaremos a recordarla",
+        "empadronamiento-paso4": "¡MUY BIEN! YA TIENES EL PADRÓN",
+    }
+}
+function getSegundoTextos() {
+    return {
+        "inicio": "",
+        "empadronamiento-paso1": " <select class='form-select p-1' onChange='cambioTextoPadron()'> <option value = 1>Bilbao</option> <option value = 2>Getxo</option>  <option value = 3>Barakaldo</option> <option value = 4>Amorebieta</option> <option value = 5>Arrigorriaga</option></select><div id='textopadron'><a href='https://www.bilbao.eus/cs/Satellite?c=BIO_Tramite_FA&categoria=temas&cid=1279100602114&language=es&menuAcordeonActivo=9&pageid=1279127425574&pagename=Bilbaonet%2FBIO_Tramite_FA%2FBIO_TramiteSrvTram&perfil=ciudadano&pestana=2&selec=1&idProc=1279101110710'>Documentos ayuntamiento</a>`</div>",
+        "empadronamiento-paso2": " <select class='form-select p-1' onChange='cambioTextoCita()'> <option value = 1>Bilbao</option> <option value = 2>Getxo</option>  <option value = 3>Barakaldo</option> <option value = 4>Amorebieta</option> <option value = 5>Arrigorriaga</option></select><div id='textopadron'><a href='https://www.bilbao.eus/cs/Satellite?c=Page&categoria=temas&cid=1279127425574&language=es&menuAcordeonActivo=9&pageid=1279127425574&pagename=Bilbaonet%2FPage%2FBIO_ListadoServiciosSrvTram&perfil=ciudadano&selec=1'>Cita ayuntamiento</a>`</div>",
+        "empadronamiento-paso3": "",
+    }
+}
+function getlinksPadron() {
+    return {
+        "1": "https://www.bilbao.eus/cs/Satellite?c=BIO_Tramite_FA&categoria=temas&cid=1279100602114&language=es&menuAcordeonActivo=9&pageid=1279127425574&pagename=Bilbaonet%2FBIO_Tramite_FA%2FBIO_TramiteSrvTram&perfil=ciudadano&pestana=2&selec=1&idProc=1279101110710",
+        "2": "https://eudala.getxo.eus/SolucionUnica/Autentificacion/EGestionInicial.aspx",
+        "3": "https://www.barakaldo.eus/OficinaVirtualBarakaldo/tramites/acceso.do?id=1",
+        "4": "https://izapideliburua.amorebieta-etxano.eus/es/258",
+        "5": "https://ayuntacity.es/cita-ayuntamiento-de-arrigorriaga/#:~:text=Solicite%20cita%20previa%20en%20el%20ayuntamiento%20de%20Arrigorriaga,online%3A%20Sede%20electr%C3%B3nica%20del%20Ayto%20P%C3%A1gina%20web%3A%20http%3A%2F%2Fwww.arrigorriaga.net",
+    }
+}
+function getlinksCita() {
+    return {
+        "1": "https://www.bilbao.eus/cs/Satellite?c=Page&categoria=temas&cid=1279127425574&language=es&menuAcordeonActivo=9&pageid=1279127425574&pagename=Bilbaonet%2FPage%2FBIO_ListadoServiciosSrvTram&perfil=ciudadano&selec=1",
+        "2": "https://getxo.eus/es/tramites/volantes",
+        "3": "https://www.barakaldo.eus/OficinaVirtualBarakaldo/tramites/acceso.do?id=1",
+        "4": "https://www.google.es/maps/place/Amorebieta+Etxanoko+Udala/@43.2201395,-2.7323911,18z/data=!4m6!3m5!1s0xd4e33b7929fbce3:0x7ea614397db160db!8m2!3d43.220276!4d-2.7322106!16s%2Fg%2F11xy_vv8f?entry=ttu",
+        "5": "https://ayuntacity.es/cita-ayuntamiento-de-arrigorriaga/#:~:text=Solicite%20cita%20previa%20en%20el%20ayuntamiento%20de%20Arrigorriaga,online%3A%20Sede%20electr%C3%B3nica%20del%20Ayto%20P%C3%A1gina%20web%3A%20http%3A%2F%2Fwww.arrigorriaga.net",
+    }
+}
+function cambioTextoPadron() {
+    document.querySelector("div#textopadron").innerHTML = `<a href='${getlinksPadron()[document.querySelector("select").value]}'>Documentos ayuntamiento</a>`
+}
+function cambioTextoCita() {
+    document.querySelector("div#textocita").innerHTML = `<a href='${getlinksCita()[document.querySelector("select").value]}'>Cita</a>`
 }
